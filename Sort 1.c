2e6a60152f398fb06b835756e350dd09236d21dc
mmint.m <pheeraya.p@ku.th>
2021-03-18 17:57:52 +0700

    แก้ไขได้100%

--- Sort 1.c
+++ Sort 1.c
@@ -4,16 +4,16 @@ int main()
 {
     int n,a,b;
     scanf("%d",&n);
-    char k[n][100],temp[100];
+    char k[n][1000],temp[1000];
     for(a=0;a<n;a++)
     {
         scanf("%s",k[a]);
     }
-for(a=0;a<n;a++)
-    {
+for(a=0;a<n-1;a++)
+   {
         for(b=a+1;b<n;b++)
         {
-            if(strcmp(k[a],k[b])>0);
+            if(strcmp(k[a],k[b])>0)
             {
                 strcpy(temp,k[a]);
                 strcpy(k[a],k[b]);
