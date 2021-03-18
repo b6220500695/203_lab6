7f35bb539cc8a92e8d1d2db340348a682076cb93
mmint.m <pheeraya.p@ku.th>
2021-03-18 17:14:22 +0700

    1

--- game.c
+++ game.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n,a,b;
+    scanf("%d",&n);
+    char k[n][1000],temp[1000];
+     for(a=0;a<n;a++)
+    {
+        scanf("%s",k[a]);
+    }
